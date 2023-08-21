b5b9e5ac2e597e7ef3ad2f29a4fbf20b169d792c
Hua Liu <hualiu@huas-air.myfiosgateway.com>
2023-08-21 13:05:15 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [showNCMovies, setShowNCMovies] = useState(false);
+  const [showKRMovies, setShowKRMovies] = useState(false);
+  const [actors, setActors] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const accessToken = 'e9d90bba-a993-4ced-bb15-e33bafcfe537';
+
+  const toggleNCMovies = () => {
+    setShowNCMovies(!showNCMovies);
+  };
+  const toggleKRMovies = () => {
+    setShowKRMovies(!showKRMovies);
+  };
+
+  // Fetch actors and movies data from APIs
+  useEffect(() => {
+    const fetchActors = async () => {
+      const response = await fetch(
+        'https://switch-yam-equator.azurewebsites.net/api/actors',
+        {
+          headers: {
+            'x-chmura-cors': accessToken,
+          },
+        }
+      );
+      const data = await response.json();
+      // console.log(data);
+      setActors(data);
+    };
+
+    const fetchMovies = async () => {
+      const response = await fetch(
+        'https://switch-yam-equator.azurewebsites.net/api/movies',
+        {
+          headers: {
+            'x-chmura-cors': accessToken,
+          },
+        }
+      );
+      const data = await response.json();
+      // console.log(data);
+      setMovies(data);
+    };
+
+    fetchActors();
+    fetchMovies();
+  }, []);
+
+  const actorMap = new Map();
+  actors.forEach((actor) => {
+    actorMap.set(actor.actorId, actor.name);
+  });
+
+  // Replace actor IDs with actor names in the movie array
+  const moviesWithActorNames = movies.map((movie) => ({
+    ...movie,
+    actors: movie.actors.map((actorId) => actorMap.get(actorId)),
+  }));
+
+  console.log(moviesWithActorNames);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Movies with Nicholas Cage and/or Keanu Reeves</h1>
+      <h2 className="title">by Hua Liu</h2>
+      <div className="movies">
+        <div>
+          <button className="nicolas" onClick={toggleNCMovies}>
+            Show Movies with Nicolas Cage
+          </button>
+          {showNCMovies && (
+            <div>
+              {moviesWithActorNames.map((movie) =>
+                movie.actors.includes('Nicolas Cage') ? (
+                  <div key={movie.movieId}>
+                    <strong>Movie Title:</strong> {movie.title}{' '}
+                    <strong>Actors:</strong>
+                    {movie.actors.join(', ')}
+                  </div>
+                ) : null
+              )}
+            </div>
+          )}
+        </div>
+        <div>
+          <button className="keanu" onClick={toggleKRMovies}>
+            Show Movies with Keanu Reeves
+          </button>
+          {showKRMovies && (
+            <div>
+              {moviesWithActorNames.map((movie) =>
+                movie.actors.includes('Keanu Reeves') ? (
+                  <div key={movie.movieId}>
+                    <strong>Movie Title:</strong> {movie.title}{' '}
+                    <strong>Actors:</strong>
+                    {movie.actors.join(', ')}
+                  </div>
+                ) : null
+              )}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
